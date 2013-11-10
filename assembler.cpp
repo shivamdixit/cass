6b86e6515c56c36b3e9793c77786cbde71577d2b
Shivam Dixit <shivamd001@gmail.com>
2013-11-10 13:49:24 +0530

    Added intrepretJZR

--- assembler.cpp
+++ assembler.cpp
@@ -8,7 +8,7 @@
 #include<string>
 #include<cstring>
 #include<cstdlib>
-
+#include<iomanip>
 
 #define INPUT_WIDTH 50					//Specifies Max length for an instruction
 #define INPUT_HEIGHT 10000				//Specifies Max number of instructions
@@ -25,11 +25,11 @@ using namespace std;
 /**
  *Global Variables
  */
-int currentIndex=0,currentRow=0,instructionLocationCounter=0;
+int currentIndex=0,currentRow=0,instructionLocationCounter=0,symbTableCount=0;
 int * asLength;
 char sourceProgram[INPUT_HEIGHT][INPUT_WIDTH];
 bool isEnd;
-
+int baseAddress=0;
 
 
 /**
@@ -45,9 +45,13 @@ void readMneumonic(ofstream &,bool );
 void mneumonicCompare(ofstream &, char * , bool );
 void interpretLDR(ofstream &, bool );
 void interpretHLT(ofstream &, bool );
+void interpretJZR(ofstream & ,bool );
+void interpretMAI(ofstream & ,bool );
+void interpretSTR(ofstream & ,bool);
 void regToBinary(ofstream &, char * );
 void hexToBinary(ofstream &,char * );
-
+int searchSymbolTable(char * );
+unsigned long long int decToBinary(int );
 
 /**
  *Structure for symbol table
@@ -61,6 +65,8 @@ struct symbol {
 typedef struct symbol symbol;
 
 symbol symbolTable[SYMB_TAB_SIZE];			//Global array to store symbol table, which is array of stuctures
+
+
 /**
  *Accepting command line arguments for input and output filename
  */
@@ -121,6 +127,7 @@ void parse(ofstream & fileOut)
 	currentRow =0;
 	currentIndex =0;
 	isEnd = false;
+	instructionLocationCounter = 0;
 	//Pass Pass
 	while(!isEnd)
 		labelScan(fileOut,false);
@@ -168,6 +175,11 @@ void insertInSymbolTable(char * name)
 {
 	int i;
 	static int index =0;
+	if(searchSymbolTable(name) != -1)  				//If symbol already exists in symbol table
+	{
+		fprintf(stderr, "Error at line number: %d\n Label Already used\n",currentRow+1);
+		exit(1);
+	}
 	symbolTable[index].ILC = instructionLocationCounter;
 	for(i=0;name[i] !=':';i++)
 	{
@@ -175,6 +187,7 @@ void insertInSymbolTable(char * name)
 	}
 	symbolTable[index].label[i] = '\0';
 	index++;
+	symbTableCount = index;
 }
 
 
@@ -207,78 +220,78 @@ void mneumonicCompare(ofstream & fileOut, char * mnemnonic, bool isFirstPass)
 		interpretMAI(fileOut,isFirstPass);
 	else if(!strcmp(mnemnonic,"JZR"))
 		interpretJZR(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"JUM"))
-		interpretJUM(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"JMC"))
-		interpretJMC(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"JMZ"))
-		interpretJMZ(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"JMP"))
-		interpretJMP(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"MVR"))
-		interpretMVR(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"ADD"))
-		interpretADD(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"SUB"))
-		interpretSUB(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"MUL"))
-		interpretMUL(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"DIV"))
-		interpretDIV(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"MOD"))
-		interpretMOD(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"OR2"))
-		interpretOR2(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"AND"))
-		interpretAND(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"XOR"))
-		interpretXOR(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"COM"))
-		interpretCOM(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"STI"))
-		interpretSTI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"NOT"))
-		interpretNOT(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"MOI"))
-		interpretMOI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"ADI"))
-		interpretADI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"SUI"))
-		interpretSUI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"MUI"))
-		interpretMUI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"DVI"))
-		interpretDVI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"MDI"))
-		interpretMDI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"ANI"))
-		interpretANI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"ORI"))
-		interpretORI(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"INC"))
-		interpretINC(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"DEC"))
-		interpretDEC(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"LHS"))
-		interpretLHS(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"RHS"))
-		interpretRHS(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"PSH"))
-		interpretPSH(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"POP"))
-		interpretPOP(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"OUT"))
-		interpretOUT(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"INP"))
-		interpretINP(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"LOP"))
-		interpretLOP(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"ELP"))
-		interpretELP(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"JUM"))
+	// 	interpretJUM(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"JMC"))
+	// 	interpretJMC(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"JMZ"))
+	// 	interpretJMZ(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"JMP"))
+	// 	interpretJMP(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"MVR"))
+	// 	interpretMVR(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"ADD"))
+	// 	interpretADD(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"SUB"))
+	// 	interpretSUB(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"MUL"))
+	// 	interpretMUL(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"DIV"))
+	// 	interpretDIV(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"MOD"))
+	// 	interpretMOD(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"OR2"))
+	// 	interpretOR2(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"AND"))
+	// 	interpretAND(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"XOR"))
+	// 	interpretXOR(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"COM"))
+	// 	interpretCOM(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"STI"))
+	// 	interpretSTI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"NOT"))
+	// 	interpretNOT(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"MOI"))
+	// 	interpretMOI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"ADI"))
+	// 	interpretADI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"SUI"))
+	// 	interpretSUI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"MUI"))
+	// 	interpretMUI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"DVI"))
+	// 	interpretDVI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"MDI"))
+	// 	interpretMDI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"ANI"))
+	// 	interpretANI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"ORI"))
+	// 	interpretORI(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"INC"))
+	// 	interpretINC(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"DEC"))
+	// 	interpretDEC(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"LHS"))
+	// 	interpretLHS(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"RHS"))
+	// 	interpretRHS(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"PSH"))
+	// 	interpretPSH(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"POP"))
+	// 	interpretPOP(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"OUT"))
+	// 	interpretOUT(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"INP"))
+	// 	interpretINP(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"LOP"))
+	// 	interpretLOP(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"ELP"))
+	// 	interpretELP(fileOut,isFirstPass);
 	else if(!strcmp(mnemnonic,"HLT"))
 		interpretHLT(fileOut,isFirstPass);
-	else if(!strcmp(mnemnonic,"NOP"))
-		interpretNOP(fileOut,isFirstPass);
+	// else if(!strcmp(mnemnonic,"NOP"))
+	// 	interpretNOP(fileOut,isFirstPass);
 	else
 	{
 		printf("Error at line number : %d . Invalid Opcode\n",currentRow+1);
@@ -309,6 +322,7 @@ void interpretLDR(ofstream & fileOut, bool isFirstPass)
 		}
 		reg[i] = '\0';
 		currentIndex++;
+		eatWhiteSpace();
 		i=0;
 		while(1)
 		{
@@ -328,7 +342,7 @@ void interpretHLT(ofstream & fileOut, bool isFirstPass)
 	char opcode[] = "00000000101000000100001010000001";
 	eatWhiteSpace();
 	instructionLocationCounter+=4;
-	if()
+
 	if(!isFirstPass)
 	{
 		fileOut<<opcode;
@@ -336,6 +350,152 @@ void interpretHLT(ofstream & fileOut, bool isFirstPass)
 	}
 	isEnd =true;
 }
+
+void interpretSTR(ofstream & fileOut,bool isFirstPass)
+{
+	int i=0;
+	char reg[3],addr[6];
+	char opcode[] = "00000000001";
+	eatWhiteSpace();
+	instructionLocationCounter+=4;
+	if(!isFirstPass)
+	{
+		while(1)
+		{
+			if(sourceProgram[currentRow][currentIndex] == ',')
+				break;
+			reg[i++] = toupper(sourceProgram[currentRow][currentIndex]);
+			currentIndex++;
+			if(i>2)					//Implement exception handling
+			{
+				printf("Error at line number : %d \n", currentRow+1);
+				exit(1);
+			}
+		}
+		reg[i] = '\0';
+		currentIndex++;
+		eatWhiteSpace();
+		i=0;
+		while(1)
+		{
+			if(sourceProgram[currentRow][currentIndex] == 'h' || sourceProgram[currentRow][currentIndex] == 'H' || sourceProgram[currentRow][currentIndex] == '\0')
+				break;
+			addr[i++] = sourceProgram[currentRow][currentIndex++];
+		}
+		fileOut<<opcode;
+		regToBinary(fileOut,reg);
+		hexToBinary(fileOut,addr);
+		fileOut<<endl;
+	}
+}
+
+void interpretMAI(ofstream & fileOut,bool isFirstPass)
+{
+	int i=0;
+	char reg[3],addr[6];
+	char opcode[] = "00000000010";
+	eatWhiteSpace();
+	instructionLocationCounter+=4;
+	if(!isFirstPass)
+	{
+		while(1)
+		{
+			if(sourceProgram[currentRow][currentIndex] == ',')
+				break;
+			reg[i++] = toupper(sourceProgram[currentRow][currentIndex]);
+			currentIndex++;
+			if(i>2)					//Implement exception handling
+			{
+				printf("Error at line number : %d \n", currentRow+1);
+				exit(1);
+			}
+		}
+		reg[i] = '\0';
+		currentIndex++;
+		eatWhiteSpace();
+		i=0;
+		while(1)
+		{
+			if(sourceProgram[currentRow][currentIndex] == 'h' || sourceProgram[currentRow][currentIndex] == 'H' || sourceProgram[currentRow][currentIndex] == '\0')
+				break;
+			addr[i++] = sourceProgram[currentRow][currentIndex++];
+		}
+		fileOut<<opcode;
+		regToBinary(fileOut,reg);
+		hexToBinary(fileOut,addr);
+		fileOut<<endl;
+	}
+}
+
+void interpretJZR(ofstream & fileOut,bool isFirstPass)
+{
+	int i=0,ILC=0;
+	char reg[3],label[LABEL_SIZE];
+	char opcode[] = "00000000011";
+	eatWhiteSpace();
+	instructionLocationCounter+=4;
+	if(!isFirstPass)
+	{
+		while(1)
+		{
+			if(sourceProgram[currentRow][currentIndex] == ',')
+				break;
+			reg[i++] = toupper(sourceProgram[currentRow][currentIndex]);
+			currentIndex++;
+			if(i>2)					//Implement exception handling
+			{
+				printf("Error at line number : %d \n", currentRow+1);
+				exit(1);
+			}
+		}
+		reg[i] = '\0';
+		currentIndex++;
+		eatWhiteSpace();
+		i=0;
+		while(1)
+		{
+			if(sourceProgram[currentRow][currentIndex] == '\0')
+				break;
+			label[i++] = sourceProgram[currentRow][currentIndex++];
+		}
+		label[i] = '\0';
+		fileOut<<opcode;
+		regToBinary(fileOut,reg);
+		ILC = searchSymbolTable(label);
+		if(ILC == -1)
+		{
+			fprintf(stderr,"Error at line number: %d\n Label Not found\n",currentRow+1);
+			exit(1);
+		}
+		fileOut<<setw(16)<<setfill('0')<<decToBinary(ILC+baseAddress);
+		fileOut<<endl;
+	}
+}
+
+int searchSymbolTable(char * element)
+{
+	int i;
+	for(i=0;i<=symbTableCount;i++)
+	{
+		if(!strcmp(symbolTable[i].label,element))
+			return symbolTable[i].ILC;
+	}
+	return -1;
+}
+
+unsigned long long int decToBinary(int num)
+{
+	unsigned long long bin=0,t=1,bit;
+	while(num)
+	{
+		bit = num % 2;
+		num = num / 2;
+		bin += bit*t;
+		t *= 10;
+	}
+	return bin;
+}
+
 void regToBinary(ofstream & fileOut, char * reg)
 {
 	const char *registr[] = {
